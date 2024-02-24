c331cdecf8a928c8120eba13c5cea5d0d3acc59a
Sashikantlvy <153577889+Sashikantlvy@users.noreply.github.com>
2024-02-24 20:33:45 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -35,7 +35,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/TheTeamInsane/TeamInsaneMusic",
+    "https://github.com/Sashikantlvy/TeamInsaneMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 
