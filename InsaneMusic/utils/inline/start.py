1f09811db2d61e25c7d24834de83cf0a2117a49f
Sashikantlvy <153577889+Sashikantlvy@users.noreply.github.com>
2024-02-24 20:29:50 +0530

    Update start.py

--- InsaneMusic/utils/inline/start.py
+++ InsaneMusic/utils/inline/start.py
@@ -26,19 +26,22 @@ def start_pannel(_):
 def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
-            InlineKeyboardButton(text=_["S_B_8"], callback_data="settings_back_helper"),
-        ],
-        [
             InlineKeyboardButton(
-                text=_["S_B_5"], url=f"https://t.me/{BOT_USERNAME}?startgroup=true"
-            ),
+                text=_["S_B_3"],
+                url=f"https://t.me/{app.username}?startgroup=true",
+            )
+        ],
+        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
+        [
+            InlineKeyboardButton(text=_["S_B_3"], callback_data="lood"),
+            InlineKeyboardButton(text=_["S_B_9"], callback_data="gib_source"),
+            
         ],
         [
-            InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER),
-            InlineKeyboardButton(text=_["S_B_4"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text=_["S_B_7"], user_id=config.OWNER_ID),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_6"], callback_data="gib_source"),
+            InlineKeyboardButton("ᴅᴏɴᴀᴛᴇ", callback_data="donate"),
         ],
     ]
     return buttons
